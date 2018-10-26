d662c41724d29a531bfdf7647f1b3fbf2bea6e38
Agung Setyawan <agung.setyawan@mirumagency.com>
2018-10-26 12:49:17 +0700

    add manifest.json

--- public/javascripts/main0.js
+++ public/javascripts/main0.js
@@ -2,5 +2,22 @@
   "use strict";
   $(window).on('load', function() {
     $('#preloader').fadeOut();
+
+    $("#bg").vegas({
+      timer: false,
+      delay: 6000,
+      transitionDuration: 2000,
+      transition: "blur",
+      slides: [{
+          src: "https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-169365.jpg"
+        },
+        {
+          src: "https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-424773.jpg"
+        },
+        {
+          src: "https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-516461.jpg"
+        }
+      ]
+    });
   });
 }(jQuery));
