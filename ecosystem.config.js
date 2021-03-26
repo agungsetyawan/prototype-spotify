f768202b97371a121fff381e618677c3dbcc1c49
Agung Setyawan <agung.01setyawan@gmail.com>
2021-03-27 01:30:56 +0700

    update domain

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -22,9 +22,9 @@ module.exports = {
       env_production: {
         NODE_ENV: 'production',
         PORT: 3001,
-        CLIENT_ID: '312f0f7a6e4e44059ed2ad2cbce2815b',
-        CLIENT_SECRET: '44cb5695e13044bcb061dfd3c9993d0f',
-        REDIRECT_URI: 'https://spotify.wkwk.fun/callback/',
+        CLIENT_ID: '229205dae1c748bf977f4575799d81b7',
+        CLIENT_SECRET: 'da07c1066a894d9fb247471d95d4a6cc',
+        REDIRECT_URI: 'https://spotify.setyawan.dev/callback/',
         DB: 'mongodb+srv://spotify:spotify@spotify-zrfvd.mongodb.net/spotify',
         GENIUS_CLIENT_ACCESS_TOKEN:
           'CuSy19nnzVQqU_vt97wk3dZo93dlKtpuk9W9pWfy8wxw-gqymD507jN8t3yJDN1l'
